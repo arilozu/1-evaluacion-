1438036ed68af443cdaafcff0baaed180671ed95
arilozu <145007265+arilozu@users.noreply.github.com>
2023-11-07 09:34:50 +0100

    tabla.py

---
+++ tabla.py
@@ -0,0 +1,15 @@
+def tabla():
+n_tabla-input( " ¿Que tabla quieres escribir?: ")
+print( "TABLLA DEL "+ str(n_tabla))
+print(str(n_tabla)+" x "+ "1 ="+str(n_tabla*1))
+print(str(n_tabla)+" × "+ "2 ="+str(n_tabla*2))
+print(str(n_tabla)+" x "+ "3 ="+str(n_tabla*3))
+print(str(n_tabla)+" x "+ "4 ="+str(n_tabla*4))
+print(str(n_tabla)+" x "+ "5 ="+str(n_tabla*5))
+print(str(n_tabla)+" x "+ "6 ="+str(n_tabla*0))
+print(str(n_tabla)+" x "+ "7 ="+str(n_tabla*7))
+print(str(n_tabla)+" x "+ "8 ="+str(n_tabla*8))
+print(str(n_tabla)+" × "+ "9 ="+str(n_tabla*9))
+print(str(n_tabla)+" × "+ "10 ="+str(n_tabla*10))
+
+tabla( )
